afd29ec1a994d6c71b34489347c5de09ed418220
dana-at-cp <dtravers@checkpoint.com>
2021-05-04 10:50:05 -0400

    DAG-1: Add GCP cloud shell deployment script

--- examples/gcloud.sh
+++ examples/gcloud.sh
@@ -43,9 +43,12 @@ gcloud iam roles create $role_id --project=$project_id --file=custom.role.yaml
 gcloud iam service-accounts create $sa_id --project=$project_id --description="The service account for the d9-autobrd-gcp cloud function" --display-name="D9 Autobrd"
 
 gcloud projects add-iam-policy-binding $project_id --member="serviceAccount:$sa_id@$project_id.iam.gserviceaccount.com" --role="projects/$project_id/roles/$role_id"
-gcloud projects add-iam-policy-binding $project_id --member="serviceAccount:$sa_id@$project_id.iam.gserviceaccount.com" --role="roles/editor"
-gcloud projects add-iam-policy-binding $project_id --member="serviceAccount:$sa_id@$project_id.iam.gserviceaccount.com" --role="roles/iam.securityAdmin"
+
+for binding_project_id in $(gcloud projects list |tail -n +2 |awk '{ print $1 }'); do
+  gcloud projects add-iam-policy-binding $binding_project_id --member="serviceAccount:$sa_id@$project_id.iam.gserviceaccount.com" --role="roles/editor"
+  gcloud projects add-iam-policy-binding $binding_project_id --member="serviceAccount:$sa_id@$project_id.iam.gserviceaccount.com" --role="roles/iam.securityAdmin"
+done
 
 gcloud functions deploy d9AutobrdOnboard --project $project_id --trigger-http --runtime nodejs10 --service-account $sa_id@$project_id.iam.gserviceaccount.com --allow-unauthenticated --max-instances 1 --timeout 540 --env-vars-file runtime.env.yaml
 
-exit 0
+exit $?
